4dbe7a7d9e19279070aa7b8731b35867cb196397
k4rma99 <k4.rma321@gmail.com>
2023-05-07 09:42:07 +0000

    Addec configuration files

--- config/server.js
+++ config/server.js
@@ -1,6 +1,6 @@
 module.exports = ({ env }) => ({
-  host: env('54.87.119.155'),
-  port: env.int('1337'),
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', '1337'),
   app: {
     keys: env.array('APP_KEYS'),
   },
