15051328aaacc614da0e21fcf68f9e41a874cd55
Ubuntu <ubuntu@ip-172-31-31-137.ec2.internal>
2023-05-14 02:21:26 +0000

    Middleware setup

--- config/middlewares.js
+++ config/middlewares.js
@@ -4,18 +4,28 @@ module.exports = ({ env }) => [
     name: 'strapi::security',
     config: {
       contentSecurityPolicy: {
+        useDefaults: true,
         directives: {
-          'script-src': ["'self'", "'unsafe-inline'", 'cdn.jsdelivr.net'],
-          'img-src': ["'self'", 'data:', 'blob:', 'cdn.jsdelivr.net', 'strapi.io', `${env('AWS_BUCKET')}.s3.${env('AWS_REGION')}.amazonaws.com`],
+          'script-sec': ['self', "'unsafe-inline'", 'cdn.jsdelivr.net'],
+          'connect-src': ['self', 'https:', 'http:'],
+          'img-src': "*",
+          'media-src': "*",
+          upgradeInsecureRequests: null,
         },
-      }
+      },
     },
   },
+];
+
+module.exports = [
+  'strapi::errors',
+  'strapi::security',
   'strapi::cors',
   'strapi::poweredBy',
   'strapi::logger',
   'strapi::query',
   'strapi::body',
+  'strapi::session',
   'strapi::favicon',
   'strapi::public',
 ];
